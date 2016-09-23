e57cce3417375fbaeacbfd0fc7a8bc6675458c75
formation <formation@TOSZ930-PORT-15>
2016-09-23 09:07:30 +0200

    MaClasse testee 2

--- src/test/java/com/diallo/zakaria/tp4/MaClasse2Test.java
+++ src/test/java/com/diallo/zakaria/tp4/MaClasse2Test.java
@@ -23,4 +23,22 @@ public class MaClasse2Test extends TestCase {
 		}
 
 	}
+	
+	public void testSommerFail() throws Exception {
+		MaClasse mc = new MaClasse(1, 1);
+
+		// cas de test 1
+		assertEquals(1, mc.sommer());
+
+		// cas de test 2
+		try {
+			mc.setA(0);
+			mc.setB(0);
+			mc.sommer();
+			fail("Une exception de type IllegalStateException aurait du etre levee");
+		} catch (IllegalStateException ise) {
+		}
+
+	}
+	
 }
